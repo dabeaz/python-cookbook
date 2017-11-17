f79056fc233f4d3b1fd7332ebae7f26b9b138e64
Zhou Yu <yuzhou03@qq.com>
2017-11-17 23:52:11 +0800

    rename file for ch04

--- src/2_string_text/sanitizing_and_cleaning_up_text/cleanup.py
+++ src/2_string_text/sanitizing_and_cleaning_up_text/cleanup.py
@@ -8,9 +8,9 @@ print(s)
 
 # (a) Remapping whitespace
 remap = {
-    ord('\t') : ' ',
-    ord('\f') : ' ',
-    ord('\r') : None      # Deleted
+    ord('\t'): ' ',
+    ord('\f'): ' ',
+    ord('\r'): None  # Deleted
 }
 
 a = s.translate(remap)
@@ -19,13 +19,13 @@ print('whitespace remapped:', a)
 # (b) Remove all combining characters/marks
 import unicodedata
 import sys
-cmb_chrs = dict.fromkeys(c for c in range(sys.maxunicode)
-                         if unicodedata.combining(chr(c)))
+
+cmb_chrs = dict.fromkeys(c for c in range(sys.maxunicode) if unicodedata.combining(chr(c)))
 
 b = unicodedata.normalize('NFD', a)
 c = b.translate(cmb_chrs)
 print('accents removed:', c)
 
 # (c) Accent removal using I/O decoding
-d = b.encode('ascii','ignore').decode('ascii')
+d = b.encode('ascii', 'ignore').decode('ascii')
 print('accents removed via I/O:', d)
