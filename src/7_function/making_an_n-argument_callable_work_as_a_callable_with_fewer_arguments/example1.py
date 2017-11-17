b5b96de3cd9fc8881e7c5edb6af485a380ef7833
Zhou Yu <yuzhou03@qq.com>
2017-11-17 23:59:11 +0800

    rename file

--- src/7_function/making_an_n-argument_callable_work_as_a_callable_with_fewer_arguments/example1.py
+++ src/7_function/making_an_n-argument_callable_work_as_a_callable_with_fewer_arguments/example1.py
@@ -1,8 +1,9 @@
 # Example of using partial() with sorting a list of (x,y) coordinates
+from functools import partial
+import math
 
 points = [ (1, 2), (3, 4), (5, 6), (7, 7) ]
 
-import math
 def distance(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
