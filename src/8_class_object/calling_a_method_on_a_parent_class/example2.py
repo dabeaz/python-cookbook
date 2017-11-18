eadc7e0f2f20df26fc697867025020497d30c75e
Zhou Yu <yuzhou03@qq.com>
2017-11-18 10:38:49 +0800

    added comment

--- src/8_class_object/calling_a_method_on_a_parent_class/example2.py
+++ src/8_class_object/calling_a_method_on_a_parent_class/example2.py
@@ -2,11 +2,13 @@ class A:
     def __init__(self):
         self.x = 0
 
+
 class B(A):
     def __init__(self):
         super().__init__()
         self.y = 1
 
+
 if __name__ == '__main__':
     b = B()
-    print(b.x, b.y)
+    print(b.x, b.y)  # 0 1
