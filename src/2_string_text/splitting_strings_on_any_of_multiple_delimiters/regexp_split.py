fcb6a461aa1399ebdb944af88829fe9010b7142c
Zhou Yu <yuzhou03@qq.com>
2017-11-18 16:35:59 +0800

    add print for regexp_split.py

--- src/2_string_text/splitting_strings_on_any_of_multiple_delimiters/regexp_split.py
+++ src/2_string_text/splitting_strings_on_any_of_multiple_delimiters/regexp_split.py
@@ -4,26 +4,41 @@
 
 import re
 
+
+def print_sep_line():
+    sep_line = "--" * 40
+    print(sep_line)
+
+
 line = 'asdf fjdk; afed, fjek,asdf,      foo'
+print("line =" + line)
+print_sep_line()
 
 # (a) Splitting on space, comma, and semicolon
 parts = re.split(r'[;,\s]\s*', line)
+print("Splitting on space, comma, and semicolon")
 print(parts)
+print_sep_line()
 
 # (b) Splitting with a capture group
+print("Splitting with a capture group")
 fields = re.split(r'(;|,|\s)\s*', line)
 print(fields)
+print_sep_line()
 
 # (c) Rebuilding a string using fields above
-values = fields[::2]
+print("Rebuilding a string using fields above")
+values = fields[::2]  # 每隔1个元素
 delimiters = fields[1::2]
 delimiters.append('')
 print('value =', values)
 print('delimiters =', delimiters)
-newline = ''.join(v+d for v,d in zip(values, delimiters))
+newline = ''.join(v + d for v, d in zip(values, delimiters))
 print('newline =', newline)
+print_sep_line()
 
 # (d) Splitting using a non-capture group
+print("Splitting using a non-capture group")
 parts = re.split(r'(?:,|;|\s)\s*', line)
 print(parts)
-
+print_sep_line()
