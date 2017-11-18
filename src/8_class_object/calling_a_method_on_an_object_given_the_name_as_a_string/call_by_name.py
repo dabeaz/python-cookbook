34b01784458afeb0f27f882ea88fac0eeaaad963
Zhou Yu <yuzhou03@qq.com>
2017-11-18 11:11:31 +0800

    refine print

--- src/8_class_object/calling_a_method_on_an_object_given_the_name_as_a_string/call_by_name.py
+++ src/8_class_object/calling_a_method_on_an_object_given_the_name_as_a_string/call_by_name.py
@@ -1,6 +1,8 @@
 # Example of calling methods by name
 
 import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -12,17 +14,19 @@ class Point:
     def distance(self, x, y):
         return math.hypot(self.x - x, self.y - y)
 
-p = Point(2,3)
+
+p = Point(4, 3)
 
 # Method 1 : Use getattr
-d = getattr(p, 'distance')(0, 0)     # Calls p.distance(0, 0)
-print(d)
+d = getattr(p, 'distance')(0, 0)  # Calls p.distance(0, 0)
+print("distance", d)
 
 # Method 2: Use methodcaller
 import operator
-d = operator.methodcaller('distance', 0, 0)(p)
-print(d)
 
+d = operator.methodcaller('distance', 0, 0)(p)
+print("distance", d)
+print()
 # Application in sorting
 points = [
     Point(1, 2),
@@ -34,7 +38,7 @@ points = [
 ]
 
 # Sort by distance from origin (0, 0)
+print("Sort by distance:")
 points.sort(key=operator.methodcaller('distance', 0, 0))
 for p in points:
     print(p)
-
