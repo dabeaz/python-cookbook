a7a881a1d6b3e506db08e6717e5482a4a3a34aaa
Zhou Yu <yuzhou03@qq.com>
2017-11-18 10:54:23 +0800

    refine print

--- src/5_file_io/adding_or_changing_the_encoding_of_an_already_open_file/file_encoding.py
+++ src/5_file_io/adding_or_changing_the_encoding_of_an_already_open_file/file_encoding.py
@@ -3,9 +3,17 @@
 import urllib.request
 import io
 
-u = urllib.request.urlopen('http://www.python.org')
+import time
+
+start = time.time()
+# u = urllib.request.urlopen('http://www.python.org')
+u = urllib.request.urlopen('http://httpbin.org/ip')
+u = urllib.request.urlopen('http://www.baidu.com')
 f = io.TextIOWrapper(u, encoding='utf-8')
 text = f.read()
 
 print(text)
+end = time.time()
+exec_time = end - start
 
+print("%d seconds used" % exec_time)
--- src/8_class_object/calling_a_method_on_a_parent_class/example3.py
+++ src/8_class_object/calling_a_method_on_a_parent_class/example3.py
@@ -9,23 +9,25 @@ class Proxy:
     # Delegate attribute assignment
     def __setattr__(self, name, value):
         if name.startswith('_'):
-            super().__setattr__(name, value)    # Call original __setattr__
+            super().__setattr__(name, value)  # Call original __setattr__
         else:
             setattr(self._obj, name, value)
+
 
 if __name__ == '__main__':
     class A:
         def __init__(self, x):
             self.x = x
-        def spam(self):
+
+        @staticmethod
+        def spam():
             print('A.spam')
+
 
     a = A(42)
     p = Proxy(a)
     print(p.x)
-    print(p.spam())
+    print(a.spam())
     p.x = 37
     print('Should be 37:', p.x)
     print('Should be 37:', a.x)
-
-    
