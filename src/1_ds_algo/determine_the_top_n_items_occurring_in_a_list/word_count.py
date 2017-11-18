1bbdf37b735984b53425e35397f5d7ae3c611c5e
Zhou Yu <yuzhou03@qq.com>
2017-11-18 08:12:10 +0800

    refine print

--- src/1_ds_algo/determine_the_top_n_items_occurring_in_a_list/word_count.py
+++ src/1_ds_algo/determine_the_top_n_items_occurring_in_a_list/word_count.py
@@ -3,23 +3,21 @@
 # Determine the most common words in a list
 
 words = [
-   'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
-   'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
-   'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
-   'my', 'eyes', "you're", 'under'
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+    'my', 'eyes', "you're", 'under'
 ]
 
 from collections import Counter
+
 word_counts = Counter(words)
 top_three = word_counts.most_common(3)
-print(top_three)
+print("top_three", top_three)
 # outputs [('eyes', 8), ('the', 5), ('look', 4)]
 
 # Example of merging in more words
 
-morewords = ['why','are','you','not','looking','in','my','eyes']
+morewords = ['why', 'are', 'you', 'not', 'looking', 'in', 'my', 'eyes', 'look', 'look']
 word_counts.update(morewords)
-print(word_counts.most_common(3))
-
-
-
+print("after merging in more words, top_three: ", word_counts.most_common(3))
