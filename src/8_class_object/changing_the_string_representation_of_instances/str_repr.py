559b69b4c5698fdef27cd79b1e4d3bf1a71c5a4e
Zhou Yu <yuzhou03@qq.com>
2017-11-18 11:13:39 +0800

    add print

--- src/8_class_object/changing_the_string_representation_of_instances/str_repr.py
+++ src/8_class_object/changing_the_string_representation_of_instances/str_repr.py
@@ -2,8 +2,14 @@ class Pair:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __repr__(self):
         return 'Pair({0.x!r}, {0.y!r})'.format(self)
+
     def __str__(self):
         return '({0.x}, {0.y})'.format(self)
 
+
+p = Pair(3, 4)
+print(p)  # 等价于print(str(p))
+print(repr(p))
