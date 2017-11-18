cf20a7f426e683d2a78e511c6f7724e1b7f0f224
Zhou Yu <yuzhou03@qq.com>
2017-11-18 08:20:49 +0800

    refine print

--- src/1_ds_algo/removing_duplicates_from_a_sequence_while_maintaining_order/remove_dup_keep_order.py
+++ src/1_ds_algo/removing_duplicates_from_a_sequence_while_maintaining_order/remove_dup_keep_order.py
@@ -1,8 +1,10 @@
 # example2.py
 #
 # Remove duplicate entries from a sequence while keeping order
+from typing import Set
 
-def dedupe(items, key=None):
+
+def dedupe(items, key=None) -> Set:
     seen = set()
     for item in items:
         val = item if key is None else key(item)
@@ -10,14 +12,16 @@ def dedupe(items, key=None):
             yield item
             seen.add(val)
 
+
 if __name__ == '__main__':
-    a = [ 
+    a = [
         {'x': 2, 'y': 3},
         {'x': 1, 'y': 4},
         {'x': 2, 'y': 3},
         {'x': 2, 'y': 3},
         {'x': 10, 'y': 15}
-        ]
-    print(a)
-    print(list(dedupe(a, key=lambda a: (a['x'],a['y']))))
-
+    ]
+    print("original list", a)
+    de_duplicate = dedupe(a, key=lambda a: (a['x'], a['y']))
+    print("deduplicate(generator): ", de_duplicate)
+    print("remove duplicate", list(dedupe(a, key=lambda a: (a['x'], a['y']))))
