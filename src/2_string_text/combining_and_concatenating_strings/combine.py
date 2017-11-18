64f0a5f24f4afe26bc959ea6ae02715e9d3ccb42
Zhou Yu <yuzhou03@qq.com>
2017-11-18 14:01:20 +0800

    add word_sep

--- src/2_string_text/combining_and_concatenating_strings/combine.py
+++ src/2_string_text/combining_and_concatenating_strings/combine.py
@@ -1,6 +1,8 @@
 # example.py
 #
 # Example of combining text via generators
+word_sep = " "
+
 
 def sample():
     yield "Is"
@@ -8,15 +10,18 @@ def sample():
     yield "Not"
     yield "Chicago?"
 
+
 # (a) Simple join operator
-text = ''.join(sample())
+text = word_sep.join(sample())
 print(text)
 
 # (b) Redirection of parts to I/O
 import sys
+
 for part in sample():
-    sys.stdout.write(part)
+    sys.stdout.write(part + word_sep)
 sys.stdout.write('\n')
+
 
 # (c) Combination of parts into buffers and larger I/O operations
 def combine(source, maxsize):
@@ -29,10 +34,9 @@ def combine(source, maxsize):
             yield ''.join(parts)
             parts = []
             size = 0
-    yield ''.join(parts)
+    yield word_sep.join(parts)
+
 
 for part in combine(sample(), 32768):
     sys.stdout.write(part)
 sys.stdout.write('\n')
-
-
