96ba45a0f475599061a670522268b890f23187e9
Zhou Yu <yuzhou03@qq.com>
2017-11-18 17:05:41 +0800

    rename class name, refine print

--- src/2_string_text/variable_interpolation_in_strings/string_formatting.py
+++ src/2_string_text/variable_interpolation_in_strings/string_formatting.py
@@ -3,27 +3,34 @@
 # Examples of variable interpolation
 
 # Class for performing safe substitutions
-class safesub(dict):
+class SafeSub(dict):
     def __missing__(self, key):
         return '{%s}' % key
+
 
 s = '{name} has {n} messages.'
 
 # (a) Simple substitution
 name = 'Guido'
 n = 37
-
+a = 99
+print("a", a)
 print(s.format_map(vars()))
+print("vars", vars())
 
 # (b) Safe substitution with missing values
-del n
-print(s.format_map(safesub(vars())))
+del n, a
+print(s.format_map(SafeSub(vars())))
+# ! print("a", a) #  NameError: name 'a' is not defined
 
 # (c) Safe substitution + frame hack
 n = 37
 import sys
+
+
 def sub(text):
-    return text.format_map(safesub(sys._getframe(1).f_locals))
+    return text.format_map(SafeSub(sys._getframe(1).f_locals))
+
 
 print(sub('Hello {name}'))
 print(sub('{name} has {n} messages'))
