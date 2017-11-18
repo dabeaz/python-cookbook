6568b2e82dba6c38b381d2200889c7e7496a600b
Zhou Yu <yuzhou03@qq.com>
2017-11-18 13:13:27 +0800

    rename file

--- src/1_ds_algo/transforming_and_reducing_data_at_the_same_time/listdir_min.py
+++ src/1_ds_algo/transforming_and_reducing_data_at_the_same_time/listdir_min.py
@@ -3,7 +3,10 @@
 # Some examples of using generators in arguments
 
 import os
-files = os.listdir(os.path.expanduser('~'))
+
+# files = os.listdir(os.path.expanduser('~'))
+files = os.listdir(os.path.abspath("."))
+print("Python files:", files)
 if any(name.endswith('.py') for name in files):
     print('There be python!')
 else:
@@ -11,14 +14,14 @@ else:
 
 # Output a tuple as CSV
 s = ('ACME', 50, 123.45)
-print(','.join(str(x) for x in s))
+print("join of iterables", ','.join(str(x) for x in s))
 
 # Data reduction across fields of a data structure
 portfolio = [
-   {'name':'GOOG', 'shares': 50},
-   {'name':'YHOO', 'shares': 75},
-   {'name':'AOL', 'shares': 20},
-   {'name':'SCOX', 'shares': 65}
+    {'name': 'GOOG', 'shares': 50},
+    {'name': 'YHOO', 'shares': 75},
+    {'name': 'AOL', 'shares': 20},
+    {'name': 'SCOX', 'shares': 65}
 ]
 min_shares = min(s['shares'] for s in portfolio)
 print(min_shares)
