6536f488a0767dabb768ce11f16fa183c6d4b7c2
Zhou Yu <yuzhou03@qq.com>
2017-11-18 10:38:25 +0800

    add main

--- src/3_date_time/finding_the_date_range_for_the_current_month/date_range.py
+++ src/3_date_time/finding_the_date_range_for_the_current_month/date_range.py
@@ -1,27 +1,30 @@
 from datetime import datetime, date, timedelta
 import calendar
 
+
 def get_month_range(start_date=None):
     if start_date is None:
         start_date = date.today().replace(day=1)
     days_in_month = calendar.monthrange(start_date.year, start_date.month)[1]
     end_date = start_date + timedelta(days=days_in_month)
-    return (start_date, end_date)
+    return start_date, end_date
 
-first_day, last_day = get_month_range()
-a_day = timedelta(days=1)
-while first_day < last_day:
-    print(first_day)
-    first_day += a_day
+if __name__ == '__main__':
 
-def daterange(start, stop, step):
-    while start < stop:
-        yield start
-        start += step
+    first_day, last_day = get_month_range()
+    a_day = timedelta(days=1)
+    while first_day < last_day:
+        print(first_day)
+        first_day += a_day
 
-for d in daterange(date(2012, 8, 1), date(2012, 8, 11), timedelta(days=1)):
-    print(d)
 
-for d in daterange(datetime(2012, 8, 1), datetime(2012, 8, 3), timedelta(minutes=30)):
-    print(d)
+    def daterange(start, stop, step):
+        while start < stop:
+            yield start
+            start += step
 
+    for d in daterange(date(2012, 8, 1), date(2012, 8, 11), timedelta(days=1)):
+        print(d)
+
+    for d in daterange(datetime(2012, 8, 1), datetime(2012, 8, 3), timedelta(minutes=60)):
+        print(d)
